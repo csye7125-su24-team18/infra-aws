f046a3876c17e652db5b7a77c2b984049618dccf
harshneu404 <agrawal.harsh@northeastern.edu>
2024-06-21 06:03:27 -0400

    feat: create kube infra

--- encryption.tf
+++ encryption.tf
@@ -13,12 +13,12 @@
 # output "ebs_volumes_kms_key_id" {
 #   value = aws_kms_key.ebs_volumes.id
 # }
-data "aws_caller_identity" "current" {}
+
 # resource "aws_kms_key" "eks_secrets_encryption" {
 #   description             = "KMS key for EKS secrets encryption"
 #   deletion_window_in_days = 10
 #    policy                  = data.aws_iam_policy_document.eks_secrets_encryption_policy.json
- 
+
 #   tags = {
 #     Name = "${var.cluster_name}-eks-secrets-encryption"
 #   }
@@ -55,24 +55,67 @@ data "aws_caller_identity" "current" {}
 #     resources = ["*"]
 #   }
 # }
-
+data "aws_caller_identity" "current" {}
+resource "aws_kms_key_policy" "eks_secrets_cmek_policy" {
+  key_id = aws_kms_key.eks_secrets_encryption.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+resource "aws_kms_key_policy" "eks_db_cmek_policy" {
+  key_id = aws_kms_key.ebs_volume_encryption.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
 resource "aws_kms_key" "eks_secrets_encryption" {
- description              = "KMS key for EKS secrets encryption"
+
+  description              = "KMS key for EKS secrets encryption"
+  deletion_window_in_days  = 10
   key_usage                = "ENCRYPT_DECRYPT"
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
 
   tags = {
-    Name = "eks-secrets-key"
+    Name = "${var.cluster_name}-eks-secrets-encryption"
   }
-
 }
- 
-# resource "aws_kms_key" "ebs_volume_encryption" {
-#   description             = "KMS key for EBS volume encryption"
-#   deletion_window_in_days = 10
- 
-#   tags = {
-#     Name = "${var.cluster_name}-ebs-volume-encryption"
-#   }
-# }
+
+
+
+
+
+output "eks_secrets_encryption_key_arn" {
+  value = aws_kms_key.eks_secrets_encryption.arn
+}
+
+
+resource "aws_kms_key" "ebs_volume_encryption" {
+  description              = "KMS key for EBS volume encryption"
+  deletion_window_in_days  = 10
+  key_usage                = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  tags = {
+    Name = "${var.cluster_name}-ebs-volume-encryption"
+  }
+}
 
