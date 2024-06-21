3a87bffd81346d20ade2c0f8f1f6064e86ad635a
harshneu404 <agrawal.harsh@northeastern.edu>
2024-06-21 06:03:38 -0400

    feat: create kube infra

--- variables.tf
+++ variables.tf
@@ -1,43 +1,43 @@
-variable "region" {
-  default = "us-east-1"
-}
+# variable "region" {
+#   default = "us-east-1"
+# }
 
-//Setup VPC variables
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-variable "vpcname" {
-  default = "terraform-vpc"
-}
+# //Setup VPC variables
+# variable "vpc_cidr" {
+#   default = "10.0.0.0/16"
+# }
+# variable "vpcname" {
+#   default = "terraform-vpc"
+# }
 
-variable "igwname" {
-  default = "terraform-igw"
-}
+# variable "igwname" {
+#   default = "terraform-igw"
+# }
 
-variable "cluster_name" {
-  default = "dev-cluster"
-}
+# variable "cluster_name" {
+#   default = "dev-cluster"
+# }
 
-variable "account_id" {
-  default = "339712789902"
-}
+# variable "account_id" {
+#   default = "339712789902"
+# }
 
-variable "cluster_version" {
-  default = "1.29"
-}
+# variable "cluster_version" {
+#   default = "1.29"
+# }
 
-variable "authentication_mode" {
-  default = "API_AND_CONFIG_MAP"
-}
+# variable "authentication_mode" {
+#   default = "API_AND_CONFIG_MAP"
+# }
 
-variable "cluster_ip_family" {
-  default = "ipv4"
-}
+# variable "cluster_ip_family" {
+#   default = "ipv4"
+# }
 
-variable "ami_type" {
-  default = "AL2_x86_64"
-}
+# variable "ami_type" {
+#   default = "AL2_x86_64"
+# }
 
-variable "capacity_type" {
-  default = "ON_DEMAND"
-}
+# variable "capacity_type" {
+#   default = "ON_DEMAND"
+# }
